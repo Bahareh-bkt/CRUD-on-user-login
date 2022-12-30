e2fafdcdd8decf1fd7b797f537f455db66b7c3b3
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-12-30 16:04:14 +0100

    create user form

--- createUser.php
+++ createUser.php
@@ -8,6 +8,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="col-sm-6">
+            <h4 class="text-center">Create User</h4>
+            <form action="createUser.php" method="post" name="form">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="submit" value="Create" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
